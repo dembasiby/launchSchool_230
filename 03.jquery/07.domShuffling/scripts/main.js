3b625c93236f58c6908977ac5f4d207c52ce6640
Demba Siby <dembasiby@gmail.com>
2018-05-03 21:37:30 +0000

    Moved variables to the top of the document

--- 03.jquery/07.domShuffling/scripts/main.js
+++ 03.jquery/07.domShuffling/scripts/main.js
@@ -1,9 +1,10 @@
 $(function () {
   var $header = $("body > header");
-  $header.prependTo("body");
-  $("main > h1").prependTo($header);
   var chinImage = $("main img").eq(1).remove();
   var babyImage = $("main img").eq(0);
+  
+  $header.prependTo("body");
+  $("main > h1").prependTo($header);
   babyImage.prependTo($("figure:nth-of-type(2)"));
   chinImage.prependTo($("figure:first-of-type"));
   $("figure").appendTo("article");
