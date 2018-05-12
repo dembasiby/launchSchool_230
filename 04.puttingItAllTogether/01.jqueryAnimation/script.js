94dde15a6b1b2955dd786cb5582b1510461d67a9
Demba Siby <dembasiby@gmail.com>
2018-05-12 11:55:10 +0000

    Configured eslint for the project

--- 04.puttingItAllTogether/01.jqueryAnimation/script.js
+++ 04.puttingItAllTogether/01.jqueryAnimation/script.js
@@ -4,15 +4,15 @@ $(function () {
   var steps = Math.round(speed / frameLength);
   var endTop = 250;
   var endLeft = 500;
-  var paragraph = document.querySelector("p"); // p has position absolute for this snippet
+  var paragraph = document.querySelector('p'); // p has position absolute for this snippet
   var i = 0;
 
   var update = function() {
-    paragraph.style.top = endTop * (i / steps) + "px";
-    paragraph.style.left = endLeft * (i / steps) + "px";
+    paragraph.style.top = endTop * (i / steps) + 'px';
+    paragraph.style.left = endLeft * (i / steps) + 'px';
     i += 1;
     i < steps && setTimeout(update, frameLength);
-  }
+  };
 
   update();  // kick off the animation loop
 });
