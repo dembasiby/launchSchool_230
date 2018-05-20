36f721593db9f96e6a4733996a43eeab02b6d17a
Demba Siby <dembasiby@gmail.com>
2018-05-20 23:13:40 +0000

    Completed the tabbed navigation

--- 04.puttingItAllTogether/03.HTMLDataAttributes/script.js
+++ 04.puttingItAllTogether/03.HTMLDataAttributes/script.js
@@ -1,3 +1,11 @@
 $(function() {
+  $('a[data-block]').on('click', function(e) {
+    e.preventDefault();
 
+    // var $element = $(this).get(0).dataset.block;
+    var $element = $(this).attr('data-block');
+    var $article = $('article[data-block='+$element+']');
+    $('article[data-block]').css('display', 'none').not($article);
+    $article.css('display', 'block');
+  });
 });
