45399701c589a7f3f57c9d95168d6cb36fbc4581
Demba Siby <dembasiby@gmail.com>
2018-05-20 20:02:53 +0000

    Completed the Js part of the animation

--- 04.puttingItAllTogether/02.animatingACartoonStrip/comic.js
+++ 04.puttingItAllTogether/02.animatingACartoonStrip/comic.js
@@ -3,11 +3,22 @@ $(function () {
   var SPEED = 250;
   var DELAY = 1500;
 
-  $blinds.each(function(i) {
-    var $blind = $blinds.eq(i);
-    $blind.delay(i * DELAY + SPEED).animate({
-      top: '+=' + $blind.height(),
-      height: 0,
-    }, SPEED);
+  function animateBlind() {
+    $blinds.each(function(i) {
+      var $blind = $blinds.eq(i);
+      $blind.delay(i * DELAY + SPEED).animate({
+        top: '+=' + $blind.height(),
+        height: 0,
+      }, SPEED);
+    });
+  }
+
+  animateBlind();
+
+  $('a').on('click', function(e) {
+    e.preventDefault();
+    $blinds.finish();
+    $blinds.removeAttr('style');
+    animateBlind();
   });
 });
