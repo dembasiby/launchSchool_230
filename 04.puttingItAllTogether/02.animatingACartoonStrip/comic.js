f0973348b4559ea10f2d0b29209af5f2d38811b6
Demba Siby <dembasiby@gmail.com>
2018-05-20 18:17:19 +0000

    Complete the first working version of the assignment

--- 04.puttingItAllTogether/02.animatingACartoonStrip/comic.js
+++ 04.puttingItAllTogether/02.animatingACartoonStrip/comic.js
@@ -1,3 +1,15 @@
 $(function () {
-  
+  var $blinds = $('[id^=blind]');
+  var SPEED = 250;
+  var DELAY = 1500;
+
+  $blinds.each(function(i) {
+    var $blind = $blinds.eq(i);
+    $blind.delay(i * DELAY).animate({
+      top: $(this).css('height').match(/[0-9]/)[0] + top,
+      height: 0
+    }, {
+      duration: SPEED,
+    });
+  });
 });
