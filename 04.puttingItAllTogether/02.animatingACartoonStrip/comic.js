19a269bef82477cedf030d212571ab15e5a21f13
Demba Siby <dembasiby@gmail.com>
2018-05-20 18:28:10 +0000

    Refactored the top property in the animate method

--- 04.puttingItAllTogether/02.animatingACartoonStrip/comic.js
+++ 04.puttingItAllTogether/02.animatingACartoonStrip/comic.js
@@ -5,11 +5,9 @@ $(function () {
 
   $blinds.each(function(i) {
     var $blind = $blinds.eq(i);
-    $blind.delay(i * DELAY).animate({
-      top: $(this).css('height').match(/[0-9]/)[0] + top,
-      height: 0
-    }, {
-      duration: SPEED,
-    });
+    $blind.delay(i * DELAY + SPEED).animate({
+      top: '+=' + $blind.height(),
+      height: 0,
+    }, SPEED);
   });
 });
