3a6bb130b102a567bf4e23d657fb1e82a3efed34
Demba Siby <dembasiby@gmail.com>
2018-06-18 17:13:41 +0000

    Initialized the main variable and the Game constructor

--- 04.puttingItAllTogether/07.guessAWord/script.js
+++ 04.puttingItAllTogether/07.guessAWord/script.js
@@ -1,11 +1,38 @@
+var $apples = $('#apples');
+var $message = $('#message');
+var $letters = $('#spaces');
+var $guesses = $('#guesses');
+    
+var randomWord = (function () {
+  var words = ['apple', 'banana', 'orange','pear', 'mango'];
 
-var words = ['apple', 'banana', 'orange','pear'];
+  return function () {
+    var idx = Math.floor(Math.random() * words.length);
+    var chosen = words[idx];
+    words.splice(idx, 1);
+    return chosen;
+  };
+}());
 
-function randomWord() {
-  var idx = Math.floor(Math.random() * words.length);
-  var chosen = words[idx];
-  words.splice(idx, 1);
-  return chosen;
+function Game() {
+  this.incorrect = 0;
+  this.letters_guessed = [];
+  this.correct_spaces = 0;
+  this.word = randomWord();
+
+  if (!this.word) {
+    this.displayMessage('Sorry, I\'ve run out of words!');
+  }
 }
+
+Game.prototype = {
+  displayMessage: function(text) {
+    $message.text(text);
+  }
+};
+
+
+
+
 
 
