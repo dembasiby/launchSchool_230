c33f50198649e3e1a068bc574e639446a384234a
Demba Siby <dembasiby@gmail.com>
2018-07-08 22:14:14 +0000

    Completed the 'Managing Collections' assessment

--- 05.AdvancedjQueryandDynamicContentCreation/02.inventory_order/inventory.js
+++ 05.AdvancedjQueryandDynamicContentCreation/02.inventory_order/inventory.js
@@ -2,39 +2,79 @@ var inventory;
 
 (function() {
   inventory = {
+    lastId: 0,
     collection: [],
     setDate: function() {
       var date = (new Date()).toGMTString();
       $('#order_date').append(date);
     },
-    template: `
-      <script type="text/template" id="inventory_item">
-        <tr>
-          <td>
-            <input type="hidden" name="item_id_ID" value="ID" />
-            <label for="item_name_ID">Name</label>
-            <input name="item_name_ID" type="text" />
-          </td>
-          <td>
-            <label for="item_stock_number_ID">Stock Number</label>
-            <input name="item_stock_number_ID" type="text" maxlength="8" />
-          </td>
-          <td>
-            <label for="item_quantity_ID">Quantity</label>
-            <input name="item_quantity_ID" type="number" value="1" />
-          </td>
-          <td>
-            <a href="#" class="delete">Delete</a>
-          </td>
-        </tr>
-      </script>
-      `,
-    init: function () {
+    cacheTemplate: function () {
+      var $iTmpl = $('#inventory_item').remove();
+      this.template = $iTmpl.html();
+    },
+    add: function() {
+      this.lastId += 1;
+
+      var item = {
+        id: this.lastId,
+        name: '',
+        stockNumber: '',
+        quantity: 1
+      };
+
+      this.collection.push(item);
+      return item;
+    },
+    update: function($item) {
+      var id = this.findID($item);
+
+      this.collection.map(function(i) {
+        if (i.id === id) {
+          i.name = $("input[name^=item_name").val();
+          i.stockNumber = $("input[name^=item_stock_number").val();
+          i.quantity = $("input[name^=item_quantity").val();
+        }
+      });
+    },
+    newItem: function(e) {
+      e.preventDefault();
+      var item = this.add();
+      var $item = $(this.template.replace(/ID/g, item.id));
+      $('#inventory').append($item);
+    },
+    findParent: function(e) {
+      return $(e.target).closest('tr');
+    },
+    findID: function($item) {
+      return +$($item).find("input[type='hidden']").val();
+    },
+    deleteItem: function(e) {
+      e.preventDefault();
+      var self = this;
+      var $item = $(this.findParent(e)).remove();
+      var id = this.findID($item);
+
+      this.collection.map(function(elem, idx) {
+        if (elem.id === id) {
+          return self.collection.splice(idx, 1);
+        };
+      });
+    },
+    updateItem: function(e) {
+      var $item = this.findParent(e);
+      this.update($item);
+    },
+    bindEvents: function() {
+      $('#add_item').on('click', $.proxy(this.newItem, this));
+      $('#inventory').on('click', 'a.delete', this.deleteItem.bind(this));
+      $('#inventory').on('blur', 'td input', this.updateItem.bind(this));
+    },
+    init: function() {
       this.setDate();
-    }
+      this.cacheTemplate();
+      this.bindEvents();
+    },
   }
 }());
 
 $(inventory.init.bind(inventory));
-console.log(inventory.template);
-
